fea5b79eb43b5207a436871f532bb019c19dcc71
GUY <guy.shanny@gmail.com>
2020-02-04 11:11:40 +0200

    hi

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from chalice import Chalice
+import time
 
 app = Chalice(app_name='helloworld')
 
 
 @app.route('/')
 def index():	
+	for i in xrange(10):
+		app.log.error("fuck")
+	time.sleep(100000)
     return {'hello': 'world'}
